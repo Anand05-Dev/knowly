e4be86d0282b2f85fd549db533d5df95224cc856
Anand Maharia <208348135+Anand05-Dev@users.noreply.github.com>
2025-08-24 14:48:48 +0530

    Update client.js

--- inngest/client.js
+++ inngest/client.js
@@ -1,4 +1,7 @@
 import { Inngest } from "inngest";
 
 // Create a client to send and receive events
-export const inngest = new Inngest({ id: "Knowly" });
+export const inngest = new Inngest({ 
+  id: "Knowly" ,
+  eventKey: process.env.INNGEST_SIGNING_KEY,
+});
